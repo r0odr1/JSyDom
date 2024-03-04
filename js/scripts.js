d1ed3a0e024975b27fbb4c7580dea1d9b756fbd5
Rodrigo <rhbon.08@gmail.com>
2024-03-04 17:09:22 -0500

    Seleccionar elementos con querySelector

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// querySelector
+const heading = document.querySelector('.header__texto h2') // retorna 0 o 1 elementos
+heading.textContent = "Nuevo Heading"
+console.log(heading);
+
+
+
+// querySelectorAll
+
+
+// getElementById
