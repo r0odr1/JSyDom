2f34e5c73d957659d9ac04705acf8e6533e72b52
Rodrigo <rhbon.08@gmail.com>
2024-03-04 17:22:47 -0500

    Seleccionar elementos con querySelectorAll

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-// querySelector
-const heading = document.querySelector('.header__texto h2') // retorna 0 o 1 elementos
-heading.textContent = "Nuevo Heading"
+// querySelector : retorna 0 o 1 elementos
+const heading = document.querySelector(".header__texto h2");
+heading.textContent = "Nuevo Heading";
 console.log(heading);
 
-
-
-// querySelectorAll
-
+// querySelectorAll : retorna de cero a todos los que concuerden con el selector
+const enlaces = document.querySelectorAll(".navegacion a");
+enlaces[0].textContent = 'Nuevo texto para Enlace'
 
 // getElementById
+const heading2 = document.getElementById('heading');
