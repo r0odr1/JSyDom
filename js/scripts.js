4974d9fb55b4faa17ac0421d587a1744530f94b5
Rodrigo <rhbon.08@gmail.com>
2024-03-06 16:32:23 -0500

    Eventos con el teclado

--- js/scripts.js
+++ js/scripts.js
@@ -31,27 +31,27 @@ console.log(nuevoEnlace);
 
 // Eventos
 
-console.log(1);
+// console.log(1);
 
-window.addEventListener("load", function () {
-  // load espera a que el JS y los archivos que dependen del HTML esten listos
-  console.log(2);
-});
+// window.addEventListener("load", function () {
+//   // load espera a que el JS y los archivos que dependen del HTML esten listos
+//   console.log(2);
+// });
 
-window.onload = function () {
-  console.log(3);
-};
+// window.onload = function () {
+//   console.log(3);
+// };
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Solo espera por el HTML, pero no espera CSS o imagenes
-  console.log(4);
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Solo espera por el HTML, pero no espera CSS o imagenes
+//   console.log(4);
+// });
 
-console.log(5);
+// console.log(5);
 
-window.onscroll = function() {
-  console.log("scrolling...");
-};
+// window.onscroll = function() {
+//   console.log("scrolling...");
+// };
 
 // Seleccionar elemntos y asociales un evento
 const btnEnviar = document.querySelector(".boton--primario");
@@ -61,6 +61,30 @@ btnEnviar.addEventListener("click", function (evento) {
 
   // Validar un formulario
 
-
   console.log("enviando formulario");
 });
+
+// Eventos de los inputs y textarea
+
+const datos = {
+  nombre: "",
+  email: "",
+  mensaje: "",
+};
+
+const nombre = document.querySelector("#nombre");
+const email = document.querySelector("#email");
+const mensaje = document.querySelector("#mensaje");
+
+nombre.addEventListener("input", leerTexto);
+email.addEventListener("input", leerTexto);
+mensaje.addEventListener("input", leerTexto);
+
+function leerTexto(e) {
+  //console.log(e.target.value);
+
+  datos[e.target.id] = e.target.value;
+
+  //console.log(e.target);
+  console.log(datos);
+}
