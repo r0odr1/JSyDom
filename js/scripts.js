29f23ef8cc78bdaeee853a07efca6249021c71d3
Rodrigo <rhbon.08@gmail.com>
2024-03-05 19:21:24 -0500

    Eventos en JS

--- js/scripts.js
+++ js/scripts.js
@@ -28,3 +28,27 @@ const navegacion = document.querySelector(".navegacion");
 navegacion.appendChild(nuevoEnlace);
 
 console.log(nuevoEnlace);
+
+// Eventos
+
+console.log(1);
+
+window.addEventListener("load", function () {
+  // load espera a que el JS y los archivos que dependen del HTML esten listos
+  console.log(2);
+});
+
+window.onload = function () {
+  console.log(3);
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Solo espera por el HTML, pero no espera CSS o imagenes
+  console.log(4);
+});
+
+console.log(5);
+
+window.onscroll = function() {
+  console.log('scrolling...');
+}
